1cecab7c03bbfdd63c50f16afd3b4ad8958aa7d3
SepulturA <39420876+MRSteeLion@users.noreply.github.com>
2022-01-25 13:14:25 +0300

    update...

--- src/App.js
+++ src/App.js
@@ -1,105 +1,248 @@
-import { useState, useCallback } from "react";
-import Grid from "./Grid";
-import Keyboard from "./Keyboard";
-import "animate.css";
+import './App.css';
+import { useEffect, useState } from 'react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Container, Snackbar, TextField, Button, Backdrop, Alert } from '@mui/material';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import styled from 'styled-components'
+import words from './words.js'
+import { Helmet } from "react-helmet";
 
-import WORD_LIST from "./words";
-// import Help from "./icons/Help";
-// import Chart from "./icons/Chart";
-// import Settings from "./icons/Settings";
-import Github from "./icons/Github";
-
-function withoutTime(dateTime = new Date()) {
-  var date = new Date(dateTime.getTime());
-  date.setHours(0, 0, 0, 0);
-  return date;
+function setCharAt(str, index, chr) {
+  if (index > str.length - 1) return str;
+  return str.substring(0, index) + chr + str.substring(index + 1);
 }
-
-const WORD_LENGTH = 5;
-
-const FINDING_WORD = WORD_LIST[withoutTime().getTime() % WORD_LIST.length];
 
 function App() {
-  const [letters, setLetters] = useState([]);
-  const [word, setWord] = useState("");
-  const [status, setStatus] = useState(null);
+  const [wordGrid, setWordGrid] = useState([])
+  const [currentRow, setCurrentRow] = useState(0)
+  const [currentCol, setCurrentCol] = useState(0)
+  const [isWon, setIsWon] = useState(false)
+  const [isLost, setIsLost] = useState(false)
+  const [doesWordExist, setDoesWordExit] = useState(true)
+  const [solution] = useState(words[Math.floor(Math.random() * words.length)])
+  // const [solution] = useState("CLEAR")
+  const [listCorrect, setListCorrect] = useState([])
+  const [listWrongPos, setListWrongPos] = useState([])
+  const [listWrong, setListWrong] = useState([])
+  const KBRow1 = "ERTYUIOPĞÜ"
+  const KBRow2 = "ASDFGHJKLŞİ"
+  const KBRow3 = "ZCVBNMÖÇ"
+  const row1 = [];
+  const row2 = [];
+  const row3 = [];
 
-  const addLetter = useCallback(
-    (letter) => {
-      setStatus(null);
-      if (word.length === WORD_LENGTH) return;
+  const Key = styled.button`
+  border-radius: 4px;
+  background-color: ${(props) => {
+      if ((listCorrect).indexOf(props.value) > -1) {
+        return ("#538d4e;")
+      } else if ((listWrongPos).indexOf(props.value) > -1) {
+        return ("#b59f3b;")
+      } else if ((listWrong).indexOf(props.value) > -1) {
+        return ("#3a3a3c")
+      }
 
-      setWord(word + letter.toLowerCase());
-    },
-    [word]
-  );
+    }}
+}
+`
 
-  const validateWord = useCallback(() => {
-    if (!WORD_LIST.includes(word)) {
-      setStatus("invalid");
-    } else if (word === FINDING_WORD) {
-      setLetters([
-        ...letters,
-        ...word.split("").map((letter) => ({ status: "correct", letter })),
-      ]);
-      setWord("");
-    } else {
-      setLetters([
-        ...letters,
-        ...word.split("").map((letter, idx) => {
-          if (FINDING_WORD[idx] === letter) {
-            return { status: "correct", letter };
-          }
-          if (
-            FINDING_WORD.indexOf(letter) >= 0 &&
-            word[FINDING_WORD.indexOf(letter)] !== letter
-          ) {
-            return { status: "missplaced", letter };
-          }
-          return { status: "wrong", letter };
-        }),
-      ]);
-      setWord("");
+  const SpecialButton = styled.button`
+  background-color: #1976d2;
+  border-radius: 4px;
+}
+`
+
+  const Box = styled.button`
+  padding: 8px;
+  max-width: 62px;
+  max-height: 62px;
+  border: ${(props) => {
+      if (props.status === "empty") {
+        return ("2px solid #3a3a3c");
+      } else if (props.status === "fill") {
+        return ("2px solid #565758");
+      }
+    }};
+  background-color: ${(props) => {
+      if (props.status === "correct") {
+        return ("#538d4e")
+      } else if (props.status === "wrongPos") {
+        return ("#b59f3b")
+      } else if (props.status === "wrong") {
+        return ("#3a3a3c")
+      } else {
+        return ("#121213")
+      }
+    }};
+  `
+
+  for (let i = 0; i < KBRow1.length; i++) {
+    row1.push(<Key className="KBLetter" value={KBRow1[i]} onClick={(e) => handleClick(e)}>{KBRow1[i]}</Key>)
+  }
+  for (let i = 0; i < KBRow2.length; i++) {
+    row2.push(<Key className="KBLetter" value={KBRow2[i]} onClick={(e) => handleClick(e)}>{KBRow2[i]}</Key>)
+  }
+  for (let i = 0; i < KBRow3.length; i++) {
+    row3.push(<Key className="KBLetter" value={KBRow3[i]} onClick={(e) => handleClick(e)}>{KBRow3[i]}</Key>)
+  }
+
+  useEffect(() => {
+
+    function initializeWordGrid() {
+      let newWordGrid = [];
+      for (let i = 0; i < 6; i++) {
+        newWordGrid.push([])
+      }
+
+      for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++) {
+          newWordGrid[i].push({ letter: "", state: "empty" })
+        }
+      }
+
+      setWordGrid(newWordGrid)
     }
-  }, [word, letters]);
+
+    if (wordGrid.length === 0) {
+      initializeWordGrid();
+    }
+  })
+
+  const handleClick = (e) => {
+    if (currentCol < 5) {
+      const newWordGrid = [...wordGrid]
+      newWordGrid[currentRow][currentCol] = { letter: e.target.value, state: "fill" }
+      setWordGrid(newWordGrid)
+      if (currentCol < 6) {
+        setCurrentCol(currentCol + 1)
+      }
+    }
+  }
+
+  const handleErase = (e) => {
+    const newWordGrid = [...wordGrid]
+    newWordGrid[currentRow][currentCol - 1] = { letter: "", state: "empty" }
+    setWordGrid(newWordGrid)
+    if (currentCol > 0) {
+      setCurrentCol(currentCol - 1)
+    }
+  }
+
+  const handleSubmit = () => {
+    if (currentCol === 5) {
+      let word = ""
+      for (let i = 0; i < 5; i++) {
+        word = word + wordGrid[currentRow][i].letter
+      }
+      if (words.includes(word.toUpperCase())) {
+        var newSolution = solution
+        for (let i = 0; i < 5; i++) {
+          if (wordGrid[currentRow][i].letter === newSolution[i]) {
+            setListCorrect(listCorrect => [...listCorrect, wordGrid[currentRow][i].letter])
+            wordGrid[currentRow][i].state = "correct"
+            newSolution = setCharAt(newSolution, i, "!")
+          }
+        }
+
+        for (let i = 0; i < 5; i++) {
+          if (wordGrid[currentRow][i].state != "correct") {
+            if (newSolution.includes(wordGrid[currentRow][i].letter)) {
+              wordGrid[currentRow][i].state = "wrongPos"
+              setListWrongPos(listWrongPos => [...listWrongPos, wordGrid[currentRow][i].letter])
+              for (let j = 0; j < newSolution.length; j++) {
+                if (newSolution[j] === wordGrid[currentRow][i].letter) {
+                  newSolution = setCharAt(newSolution, j, "?")
+                  break;
+                }
+              }
+            } else {
+              setListWrong(listWrong => [...listWrong, wordGrid[currentRow][i].letter])
+              wordGrid[currentRow][i].state = "wrong"
+            }
+          }
+        }
+        if (word === solution) {
+          setIsWon(true)
+        } else if (currentRow > 4) {
+          setIsLost(true)
+        } else {
+          setCurrentRow(currentRow + 1)
+          setCurrentCol(0)
+        }
+      } else {
+        setDoesWordExit(false)
+      }
+    }
+  }
+
+  const handleClose = () => {
+    setIsLost(false);
+    setIsWon(false);
+    setDoesWordExit(true)
+  };
 
   return (
-    <>
-      <main className="flex flex-col justify-between items-center h-[100vh] max-w-[500px] m-auto">
-        <header className="flex flex-row justify-center items-center p-1 min-w-full border-b border-neutral-300">
-          {/* <Help className="text-neutral-500" /> */}
-          <h1 className="uppercase font-bold text-4xl tracking-wider">
-            WORDLE (TR)
-          </h1>
-          {/* <div className="flex gap-1">
-          <Chart className="text-neutral-500" />
-          <Settings className="text-neutral-500" />
-        </div> */}
-        </header>
-        <Grid
-          letters={[
-            ...letters,
-            ...word.split("").map((letter) => ({ status, letter })),
-          ]}
-        />
-
-        <Keyboard
-          onKeyPressed={addLetter}
-          onEnter={validateWord}
-          onBackspace={() => setWord(word.slice(0, -1))}
-          letters={letters}
-        />
-      </main>
-      <a
-        href="https://github.com/MRSteeLion"
-        title="Source code"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Github className="absolute right-2 top-2" />
-      </a>
-    </>
+    <div>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+      </Helmet>
+      <Container maxWidth="sm">
+        <div className="Header">WORDLE CLONE TÜRKÇE</div>
+        <div className="App">
+          <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={isWon}
+            onClick={handleClose}>
+            You Won!
+          </Backdrop>
+          <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={isLost}
+            onClick={handleClose}>
+            You Lost!
+            The answer was {solution}
+          </Backdrop>
+          <div className="Grid">
+            {wordGrid.map((row, rowIndex) => (
+              <div className="Row">
+                {row.map((col, colIndex) => (
+                  <Box status={wordGrid[rowIndex][colIndex].state}>
+                    <div className="Letter">
+                      {wordGrid[rowIndex][colIndex].letter}
+                    </div>
+                  </Box>
+                ))
+                }
+              </div>
+            ))}
+          </div>
+          <Snackbar open={!doesWordExist} autoHideDuration={4000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            Kelime dağarcığımızda böyle bir kelime bulunmuyor.
+            </Alert>
+          </Snackbar>
+          <div className="Keyboard">
+            <div className="KBRow">
+              {row1}
+            </div>
+            <div className="KBRow">
+              {row2}
+            </div>
+            <div className="KBRow">
+              <SpecialButton onClick={(e) => handleSubmit(0)}>ENTER</SpecialButton>
+              {row3}
+              <SpecialButton onClick={(e) => handleErase(e)}><BackspaceIcon /></SpecialButton>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
+
+
 
 export default App;
